690af80c77d71cbd40884ea776fd0fa32a892f22
wubmu <923185028@qq.com>
2022-08-02 16:09:43 +0800

    功能5 hook机制

---
+++ 5-hooks/generator/caluse.go
@@ -0,0 +1,49 @@
+package clause
+
+import "strings"
+
+type Clause struct {
+	sql     map[Type]string        // 子sql的类型
+	sqlVars map[Type][]interface{} // 子sql的vars
+}
+
+type Type int
+
+// Support types for Clause
+const (
+	INSERT Type = iota
+	VALUES
+	SELECT
+	LIMIT
+	WHERE
+	ORDERBY
+	UPDATE
+	DELETE
+	COUNT
+)
+
+//Set 方法根据 Type 调用对应的 generator，生成该子句对应的 SQL 语句。
+//Build 方法根据传入的 Type 的顺序，构造出最终的 SQL 语句。
+func (c *Clause) Set(name Type, vars ...interface{}) {
+	if c.sql == nil {
+		c.sql = make(map[Type]string)
+		c.sqlVars = make(map[Type][]interface{})
+	}
+
+	sql, vars := generators[name](vars...)
+	c.sql[name] = sql
+	c.sqlVars[name] = vars
+}
+
+func (c *Clause) Build(orders ...Type) (string, []interface{}) {
+	var sqls []string
+	var vars []interface{}
+	for _, order := range orders {
+		if sql, ok := c.sql[order]; ok {
+			sqls = append(sqls, sql)
+			vars = append(vars, c.sqlVars[order]...)
+		}
+	}
+
+	return strings.Join(sqls, " "), vars
+}
---
+++ 5-hooks/session/hooks.go
@@ -0,0 +1,44 @@
+package session
+
+import (
+	"orm/log"
+	"reflect"
+)
+
+// Hooks constants
+const (
+	BeforeQuery  = "BeforeQuery"
+	AfterQuery   = "AfterQuery"
+	BeforeUpdate = "BeforeUpdate"
+	AfterUpdate  = "AfterUpdate"
+	BeforeDelete = "BeforeDelete"
+	AfterDelete  = "AfterDelete"
+	BeforeInsert = "BeforeInsert"
+	AfterInsert  = "AfterInsert"
+)
+
+// CallMethod 注册hooks机制
+//
+// CallMethod 注册hooks机制
+//  @Description:
+//  @auth wyb  2022-08-02 15:56:01
+//  @receiver s
+//  @param method 将 s *Session 作为入参调用。每一个钩子的入参类型均是 *Session。
+//  @param value
+//
+func (s *Session) CallMethod(method string, value interface{}) {
+	fm := reflect.ValueOf(s.RefTable().Model).MethodByName(method)
+	if value != nil {
+		fm = reflect.ValueOf(value).MethodByName(method)
+	}
+
+	param := []reflect.Value{reflect.ValueOf(s)}
+	if fm.IsValid() {
+		if v := fm.Call(param); len(v) > 0 {
+			if err, ok := v[0].Interface().(error); ok {
+				log.Error(err)
+			}
+		}
+	}
+	return
+}
